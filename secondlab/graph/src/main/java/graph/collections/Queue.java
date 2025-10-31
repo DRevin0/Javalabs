3e519034c788ee5b41aad6ad4c0aab00c934ed5c
DRevin0 <dimareva2000@gmail.com>
2025-10-31 14:06:49 +0300

    Добавил очередь для дальнейшего использовании в BFS, измненил структуру

---
+++ secondlab/graph/src/main/java/graph/collections/Queue.java
@@ -0,0 +1,58 @@
+package collections;
+public class Queue<T>{
+    private T[] array;
+    private int head;
+    private int tail;
+    private int size;
+    private int capacity;
+    public Queue(){
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+        this.capacity = 10;
+        this.array = (T[]) new Object[capacity];
+    }
+
+    public void enqueue(T element){
+        if(size == capacity){
+            T[] newArray = (T[]) new Object[capacity*2];
+            for(int i = 0; i< size;i++){
+                newArray[i] = array[(head+i)%capacity];
+            }
+            array = newArray;
+            head = 0;
+            tail = size;
+        }
+        array[tail] = element;
+        tail = (tail+1)%capacity;
+        size++;
+    }
+    public T dequeue(){
+        if(size == 0){
+            throw new IllegalStateException("Queue is empty");
+        }
+        T element = array[head];
+        array[head] = null;
+        head = (head + 1)% capacity;
+        size--;
+        return element;
+    }
+    public T peek(){
+        if(size == 0){
+            throw new IllegalStateException("Queue is empty");
+        }
+        return array[head];
+    }
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    
+
+}
